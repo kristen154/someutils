e6560444967f01620305328292deeaa1cb582dd1
kristen154 <kristen154@users.noreply.github.com>
2018-06-29 14:23:44 +0800

    解决保存中文文件名时乱码问题

--- QRCODEimport/upload.php
+++ QRCODEimport/upload.php
@@ -3,6 +3,7 @@
 	define('UPLOAD_DIR', 'images/');
 	$img = $_POST['img'];
 	$file=$_POST['filename'];
+	$file=iconv("UTF-8","gb2312",$file);//解决保存中文文件名时乱码问题，目前只在win7里测试
 	//var_dump($img)
 	$img = str_replace('data:image/png;base64,', '', $img);
 	$img = str_replace(' ', '+', $img);
